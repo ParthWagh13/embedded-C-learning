9708b0312cfcbd47c189ae30de70d393dc986d27
Parth <parthwagh13@gmail.com>
2026-05-10 19:23:09 +0530

    Made number reverser without splitting

---
+++ Chapter4-Expressions/Number reverser without splitting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int i,j,k;
+    printf("Enter a 3 digit number: ");
+    scanf("%1d%1d%1d", &i,&j,&k);
+
+    printf("The reverse number is %d%d%d",k,j,i);
+
+    return 0;
+}
