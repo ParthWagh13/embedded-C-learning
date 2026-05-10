7a7656edd71758b66c07703eca43be7b24593f39
Parth <parthwagh13@gmail.com>
2026-05-10 19:07:14 +0530

    Upgraded the 2 digit reversal to 3 digit reversal program

--- Chapter4-Expressions/Number reverser.c
+++ Chapter4-Expressions/Number reverser.c
@@ -3,15 +3,17 @@
 int main(void)
 
 {
-    int number,tens,units;
+    int number,hundreds,tens,tens1,units;
 
-    printf("Enter a two-digit number: ");
+    printf("Enter a three-digit number: ");
     scanf("%d",&number);
 
+    hundreds = number/100;
     units = number % 10;
     tens = number/10;
+    tens1 = tens%10;
 
-    printf("The reversal is %d%d", units,tens);
+    printf("The reversal is %d%d%d", units,tens1,hundreds);
 
     return 0;
 }
