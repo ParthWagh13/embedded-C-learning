567f29fc3b15decb38806a4d374426dc3d81a151
Parth <parthwagh13@gmail.com>
2026-05-12 19:16:52 +0530

    Made a program that utilizes switch statement and prints out a dated invite

--- Chapter5-Selection_Statements/broker.c
+++ Chapter5-Selection_Statements/broker.c
@@ -9,17 +9,17 @@ int main(void)
     scanf("%d", &value);
 
     if (value > 500000){
-        commission = 255 + (0.09 * value)/100;
+        commission = 255.00f + (0.09f * value)/100.00f;
     }else if (value > 50000){
-        commission = 155 + (0.11 * value)/100;
+        commission = 155.00f + (0.11f * value)/100.00f;
     }else if (value > 25000){
-        commission = 100 + (0.22 * value)/100;
+        commission = 100.00f + (0.22f * value)/100.00f;
     }else if (value > 6250){
-        commission = 76 + (0.34 * value)/100;
+        commission = 76.00f + (0.34f * value)/100.00f;
     }else if (value > 2500){
-        commission = 56 + (0.66 * value)/ 100;
-    }else if (value < 2500){
-        commission = 30  + (value * 1.7)/100;
+        commission = 56.00f + (0.66f * value)/ 100.00f;
+    }else if (value >= 2500){
+        commission = 30.0f  + (value * 1.7f)/100.0f;
     }if (commission < 39){
         commission = 39;
     }
