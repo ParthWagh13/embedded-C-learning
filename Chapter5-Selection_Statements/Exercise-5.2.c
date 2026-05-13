56afa15eabef7a64f2f2b45596f6fbf268ffca28
ParthWagh13 <parthwagh@Wagh.localdomain>
2026-05-13 05:12:18 +0000

    Simplified a complex if statement into a one line statement

---
+++ Chapter5-Selection_Statements/Exercise-5.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int age, teenager;
+
+	printf("Enter your age: ");
+	scanf("%d", &age);
+
+	teenager = (age>= 13 && age <=19);
+
+	printf("Teenager status: %d\n", teenager);
+
+	return 0;
+
+}
