87412ad55dc2ccef9a733250e942ab03d38723a7
ParthWagh13 <parthwagh13@gmail.com>
2026-06-11 07:58:38 +0000

    Used break statement for a program which finds prime numbers

--- Chapter6-Loops/practice-loops.c
+++ Chapter6-Loops/practice-loops.c
@@ -2,9 +2,18 @@
 
 int main(void)
 {
-	int i=10;
-	while (i > 0) {
-		printf("T minus %d and counting", i--);
-		}
+	int d,n;
+
+	printf("Enter a number: ");
+	scanf("%d", &n);
+
+	for(d = 2 ; d < n ; d++)
+		if (n % d == 0)
+		  break;
+	if (d < n)
+		printf("%d is divisible by %d\n",n,d);
+	else
+		printf("%d is prime\n", n);
+
 	return 0;
 }
